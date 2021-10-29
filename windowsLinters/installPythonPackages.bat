477c7b4d526406d8d534365e2258e81a4eed85e9
= <=>
2021-10-29 13:18:17 +0200

    Removed no longer needed installation script. Better do it manually (see README.md in Condifure ALE to make powerfull IDE section)

--- windowsLinters/installPythonPackages.bat
+++
@@ -1,6 +0,0 @@
-@echo Preparing to install python dependencies to use ALE linters...
-@echo Reading content of this folder and trying to skip current file:
-for %%a in (*) do if not %%~na == %~n0 (start pip install %%~na)
-
-@Echo ... Done
-Pause
