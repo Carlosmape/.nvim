1eaed769977924a4e4b73b6a5bf572b218456614
Carlosmape <cagrmape@gmail.com>
2023-03-01 19:25:52 +0100

    Added comment to do not forget it!

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -4,6 +4,10 @@ if not present then
 	return
 end
 
+-----------------------------------------------------------
+-- This plugin is automatically configured by mason-nvim --
+-----------------------------------------------------------
+
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
